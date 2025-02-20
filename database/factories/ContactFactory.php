59e146818e23cc44b22e50ab5ea8b69473f2d9ef
miguel <miigueel9219@gmail.com>
2025-02-20 12:21:05 +0100

    corregir contactfactory

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -3,21 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
  */
 class ContactFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->value('id') ?? null,
+            'message' => $this->faker->paragraph(3),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
